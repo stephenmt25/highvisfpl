5ec519a04b9609f3a0b0f7a87f21651d8352fdb3
stephenmt <stephenmthomas95@gmail.com>
2025-01-08 00:47:24 +0530

    supabase db integration

--- src/components/averagePointsCard.tsx
+++ src/components/averagePointsCard.tsx
@@ -7,17 +7,29 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { TrendingDown } from "lucide-react"
+import { createClient } from '@/utils/supabase/client'
+import { useEffect, useState } from 'react'
 
 
 
 export function AveragePointsCard() {
+  const [week, setWeek] = useState<any[] | null>(null)
+  const supabase = createClient()
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await supabase.from('fploveralldata').select()
+      setWeek(data)
+    }
+    getData()
+  }, [])
+
   return (
         <Card className="">
           <CardHeader className="pb-1" >
             <CardDescription className="">Average GW Points</CardDescription>
           </CardHeader>
           <CardContent className="pb-2">
-            <h1 className=" text-3xl">56</h1>
+            <h1 className=" text-3xl">{week?.[19]?.average_entry_score || 0}</h1>
           </CardContent> 
           <CardFooter className="flex text-sm  gap-2 leading-none">
             down by 8.3% <TrendingDown className="h-4 w-4" />
